74fc4ae7a8734abbd832ad10e67913239a23da31
henrique <henriqueunipe@gmail.com>
2024-02-16 18:14:44 -0300

    first commit

---
+++ Marvel.API/Controllers/MarvelController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Marvel.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarvelController : ControllerBase
+    {
+    }
+}
